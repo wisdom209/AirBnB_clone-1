44b2b080efffb0cc0080c51d828f5359f965ba44
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-03-22 17:12:12 +0100

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,13 +26,17 @@ class FileStorage:
 
     def all(self, cls=None):
         """returns the dictionary __objects"""
+        """Returns a dictionary of models currently in storage"""
         if cls is not None:
-            new_dict = {}
-            for key, value in self.__objects.items():
-                if cls == value.__class__ or cls == value.__class__.__name__:
-                    new_dict[key] = value
-            return new_dict
-        return self.__objects
+            temp = {}
+            new_obj = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                if key.startswith(cls.__name__) is True:
+                    new_obj.update({key: val})
+            return new_obj
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
@@ -59,11 +63,11 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """delete obj from __objects if it’s inside"""
-        if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
-            if key in self.__objects:
-                del self.__objects[key]
+        """deletes value from object"""
+        key = obj.to_dict()['__class__'] + '.' + obj.id
+        if key in self.__objects:
+            self.all().pop(obj.to_dict()['__class__'] + '.' + obj.id)
+        self.save()
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
