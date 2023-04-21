4b1ee3debe0b3a47e6787c0e1d54d5819f6fe31d
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-04-21 19:51:24 +0100

    Feat: Added 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,12 +3,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def display_hellohbnb():
     """ display Hello HBNB """
     return "Hello HBNB!"
 
+
 if __name__ == '__main__':
     """start app"""
     app.run(host='0.0.0.0', port=5000)
-
