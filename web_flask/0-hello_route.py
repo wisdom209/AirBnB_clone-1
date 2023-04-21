848a4dd8f587b2157d1f611197131abb8a512ac6
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-04-21 19:45:50 +0100

    Feat: Added 0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Starts a flask app """
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def display_hellohbnb():
+    """ display Hello HBNB """
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    """start app"""
+    app.run(host='0.0.0.0', port=5000)
