daa6a654cb80d96d08cddbc0cd9dedecca4e42fa
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-04-21 20:35:53 +0100

    Feat: Added 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Starts a flask app """
+from flask import Flask, abort
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hellohbnb():
+    """ display Hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """ display HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_is_text(text):
+    """ display c <text> """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python_text(text='is cool'):
+    """ display Python  <text> """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def display_number_is_a_number(n):
+    """display n is a number only if n is a Number"""
+    try:
+        n = int(n)
+        return "{} is a number".format(n)
+    except Exception:
+        abort(404)
+
+
+if __name__ == '__main__':
+    """start app"""
+    app.run(host='0.0.0.0', port=5000)
