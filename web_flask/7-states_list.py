a60dc248c1f30cccc93257f26fbed08070ab49d6
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-04-22 19:20:42 +0100

    List states in db added

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-""" Script that starts flask web application """
-from models import *
-from models import storage
+"""Script that starts a Flask app"""
 from flask import Flask, render_template
+from models import storage
+
 app = Flask(__name__)
 
 
-@app.route('/state_list', strict_slashes=False)
-def get_state_list():
-    """function defines the /state_list route"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    """list the state elements"""
+    states = storage.all("State").values()
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_database(exception):
-    """tear down db"""
+def teardown(except):
+    """remove the currentSQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == '__main__':
-    """run the app"""
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
