eebea091e3f81ea2e3f4add32bba4d1289b341d7
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-04-22 20:00:20 +0100

    List city states in db added

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Script that starts a Flask app"""
+"""starts a Flask web application"""
+
 from flask import Flask, render_template
 from models import storage
-
 app = Flask(__name__)
 
 
-@app.route("/cities_by_state", strict_slashes=False)
-def state_city_list():
-    """list the state elements"""
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display the states and cities listed in alphabetical order"""
     states = storage.all("State").values()
-    return render_template("8-states_list.html", states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(e):
-    """remove the currentSQLAlchemy Session"""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
